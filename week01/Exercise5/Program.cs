6b2db8761eff9a1ed6e63962b62fac573bfaf8df
Idowu5407 <juniormicheal25@gmail.com>
2025-05-15 04:26:51 +0100

    Exercise 05 completed

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -3,7 +3,51 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
-    }
+        {
+            // Call the WelcomeMessage method
+            WelcomeMessage();
+            
+            // Prompt the user for their name
+            string userName = PromptUserName();
+            
+            // Prompt the user for their favorite number
+            int favoriteNumber = PromptUserNumber();
+            
+            // Calculate the square of the favorite number
+            int squaredNumber = Square(favoriteNumber);
+            
+            // Display the result
+            DisplayResult(userName, squaredNumber);
+        }
+        // Display a welcome message
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+    
+        // Prompt the user for name 
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name:");
+            return Console.ReadLine();
+        }
+        
+        // Prompt the user for favorite number
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number:");
+            return int.Parse(Console.ReadLine());
+        }
+        
+        // Accepts an interger as a parameter and returns the square of that number
+        static int Square(int number)
+        {
+            return number * number;
+        }
+        static void DisplayResult(string userName, int squaredNumber)
+        {
+            Console.WriteLine($"Hello {userName}, the square of your favorite number is: {squaredNumber}");
+        }
+       
+    
 }
