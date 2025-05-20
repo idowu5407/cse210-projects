a448e0ca3c05f5e12503ba66a072f88ef56e6b50
Idowu5407 <juniormicheal25@gmail.com>
2025-05-20 08:33:29 +0100

    Journal design create

---
+++ week02/Resumes/Journal.cs
@@ -0,0 +1,25 @@
+public class Journal
+{
+    private List<Entry> entries;
+
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+    public void DisplayAll()
+    {
+
+    }
+    
+    {
+        public void SaveToFile(string file)
+    {
+        // Code to save entries to a file
+    }
+        public void LoadFromFile(string file)
+    {
+        // Code to load entries from a file
+    }
+
+    
+}
