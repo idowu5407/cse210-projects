bc2755c039b0ec588047a046fdee86068b03b7f9
Idowu5407 <juniormicheal25@gmail.com>
2025-06-15 22:35:57 +0100

    Polymorphism practise

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Create individual instances and test them
+        Square square = new Square("Red", 4);
+        Console.WriteLine($"Square Color: {square.GetColor()}, Area: {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Blue", 5, 3);
+        Console.WriteLine($"Rectangle Color: {rectangle.GetColor()}, Area: {rectangle.GetArea()}");
+
+        Circle circle = new Circle("Green", 2.5);
+        Console.WriteLine($"Circle Color: {circle.GetColor()}, Area: {circle.GetArea()}");
+
+        // List of Shapes
+        List<Shape> shapes = new List<Shape>
+        {
+            square,
+            rectangle,
+            circle
+        };
+
+        Console.WriteLine("\nIterating through list of shapes:");
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}, Area: {shape.GetArea()}");
+        }
     }
 }
