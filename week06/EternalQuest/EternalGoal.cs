e1d2aa6c245c83216c5329c3a4fb008e854000b6
Idowu5407 <juniormicheal25@gmail.com>
2025-06-18 23:51:47 +0100

    Eternal Quest progam done

---
+++ week06/EternalQuest/EternalGoal.cs
@@ -0,0 +1,13 @@
+class EternalGoal : Goal 
+{
+    // Represents an eternal goal in the Eternal Quest game
+    public EternalGoal(string name, string description, int points) : base(name, description, points) { }
+
+    public override int RecordEvent() => _points;
+
+    public override bool IsComplete() => false;
+
+    public override string GetStringRepresentation() => $"EternalGoal|{_shortName}|{_description}|{_points}";
+
+}
+
--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,9 +1,16 @@
 using System;
 
-class Program
+// I add level up functionality to the Eternal Quest game
+// Goal deletion feature
+// Visual point and level feedback in menu
+
+partial class Program
 {
+    // Main entry point for the Eternal Quest game
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        GoalManager manager = new GoalManager();
+        manager.Start();
     }
+ 
 }
