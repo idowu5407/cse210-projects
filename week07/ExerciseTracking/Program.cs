ce0bd6cd33ff0102ac54540b99ecc28d8c08fa2e
Idowu5407 <juniormicheal25@gmail.com>
2025-06-19 02:45:33 +0100

    screenshot added

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,78 +5,18 @@ class Program
 {
     static void Main()
     {
+        DateTime today = DateTime.Now.Date;
+
         List<Activity> activities = new List<Activity>
         {
-            new Running(new DateTime(2025, 11, 3), 34, 4.8),
-            new Cycling(new DateTime(2025, 11, 3), 30, 9.7),
-            new Swimming(new DateTime(2025, 11, 3), 20, 20)
+            new Running(today, 30, 4.8),
+            new Cycling(today, 30, 9.7),
+            new Swimming(today, 30, 20)
         };
 
         foreach (Activity activity in activities)
         {
             Console.WriteLine(activity.GetSummary());
         }
-    }
-}
-
-abstract class Activity
-{
-    public DateTime Date { get; }
-    public int Duration { get; } // in minutes
-
-    public Activity(DateTime date, int duration)
-    {
-        Date = date;
-        Duration = duration;
-    }
-
-    public abstract string GetSummary();
-}
-
-class Running : Activity
-{
-    public double Distance { get; } // in kilometers
-
-    public Running(DateTime date, int duration, double distance)
-        : base(date, duration)
-    {
-        Distance = distance;
-    }
-
-    public override string GetSummary()
-    {
-        return $"{Date.ToShortDateString()} Running ({Duration} min): Distance {Distance} km";
-    }
-}
-
-class Cycling : Activity
-{
-    public double Speed { get; } // in kph
-
-    public Cycling(DateTime date, int duration, double speed)
-        : base(date, duration)
-    {
-        Speed = speed;
-    }
-
-    public override string GetSummary()
-    {
-        return $"{Date.ToShortDateString()} Cycling ({Duration} min): Speed {Speed} kph";
-    }
-}
-
-class Swimming : Activity
-{
-    public int Laps { get; }
-
-    public Swimming(DateTime date, int duration, int laps)
-        : base(date, duration)
-    {
-        Laps = laps;
-    }
-
-    public override string GetSummary()
-    {
-        return $"{Date.ToShortDateString()} Swimming ({Duration} min): Laps {Laps}";
     }
 }
